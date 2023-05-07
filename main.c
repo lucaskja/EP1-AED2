ba66c6d113e8bfba421da0b97edfaae916d7f16e
lucaskja <lucaskja@usp.br>
2023-05-07 11:24:24 -0300

    Add cabeçalho

--- main.c
+++ main.c
@@ -1,3 +1,16 @@
+/*********************************************************************/
+/**   ACH2024 - Algoritmo e Estrutura de Dados II                   **/
+/**   EACH-USP - Primeiro Semestre de 2022                          **/
+/**   94 - Karina Valdivia Delgado                                  **/
+/**                                                                 **/
+/**   Primeiro Exercicio-Programa                                   **/
+/**                                                                 **/
+/**   Lucas Kledeglau Jahchan Alves              13732182           **/
+/**    Guilherme Campos Silva Lemes Prestes      13720460           **/
+/**    Vinicius Kazuo Inagaki                    13747967           **/
+/**                                                                 **/
+/*********************************************************************/
+
 #include "./funcionalidades/saida.c"
 
 int main(){
